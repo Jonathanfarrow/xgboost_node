7ac12c145c6f7f6fea3f89350ee11a0c653c5d80
Jonathan Farrow <Jonathanjamesf@gmail.com>
2024-12-10 22:04:06 +0100

    os darwin added

--- scripts/OSDetect.js
+++ scripts/OSDetect.js
@@ -1,19 +1,23 @@
-const fs = require('fs');
-const path = require('path');
+import { existsSync, unlinkSync, copyFileSync } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const platform = process.platform;
 if (platform === 'darwin') {
-    const libPath = path.join(__dirname, '../lib');
-    const binaryPath = path.join(__dirname, '../binary');
+    const libPath = join(__dirname, '../lib');
+    const binaryPath = join(__dirname, '../binary');
     
     // Remove the Linux .so file
-    const linuxLib = path.join(libPath, 'libxgboost.so');
-    if (fs.existsSync(linuxLib)) {
-        fs.unlinkSync(linuxLib);
+    const linuxLib = join(libPath, 'libxgboost.so');
+    if (existsSync(linuxLib)) {
+        unlinkSync(linuxLib);
     }
     
     // Copy the Mac .dylib file to lib folder
-    const macSource = path.join(binaryPath, 'libxgboost.dylib');
-    const macDest = path.join(libPath, 'libxgboost.dylib');
-    fs.copyFileSync(macSource, macDest);
+    const macSource = join(binaryPath, 'libxgboost.dylib');
+    const macDest = join(libPath, 'libxgboost.dylib');
+    copyFileSync(macSource, macDest);
 }
